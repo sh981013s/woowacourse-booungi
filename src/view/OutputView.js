e70148bc407308aaf4432670957693eeb406c7a6
sh981013s <sh981013s@gmail.com>
2022-12-16 22:27:51 +0900

    feat(OutputView): 각 케이스 결과 출력 로직 구현

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,8 +1,21 @@
 const { Console } = require("@woowacourse/mission-utils");
+const { CASE_RESULT } = require("../constants/Messages");
+const DashMaker = require("../utils/DashMaker");
 
 const OutputView = {
   printError(errorLog) {
     Console.print(errorLog.message);
+  },
+
+  printCaseResult(currentCarsInformation) {
+    Console.print(CASE_RESULT.MESSAGE);
+    for (const singleCarInformation of currentCarsInformation) {
+      Console.print(
+        `${singleCarInformation.name} : ${DashMaker.getDash(
+          singleCarInformation.movement,
+        )}\n`,
+      );
+    }
   },
 };
 
