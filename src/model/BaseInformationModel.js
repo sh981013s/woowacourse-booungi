360ca802b91067762c52e7aae7776db7d964bd76
sh981013s <sh981013s@gmail.com>
2022-12-16 21:18:29 +0900

    feat(BaseInformationModel): 차이름, 시도횟수 필드 추가 및 getter/ setter 구현

--- src/model/BaseInformationModel.js
+++ src/model/BaseInformationModel.js
@@ -1,5 +1,27 @@
 class BaseInformationModel {
-  constructor() {}
+  #carNames;
+  #tryCount;
+
+  constructor() {
+    this.#carNames = [];
+    this.#tryCount = null;
+  }
+
+  getCarNames() {
+    return this.#carNames;
+  }
+
+  setCarNames(newCarNames) {
+    this.#carNames = newCarNames;
+  }
+
+  getTryCount() {
+    return this.#tryCount;
+  }
+
+  setTryCount(newTryCount) {
+    this.#tryCount = newTryCount;
+  }
 }
 
 module.exports = BaseInformationModel;
