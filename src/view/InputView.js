d93c3c915d44594883883b1922e3faaec6049e26
sh981013s <sh981013s@gmail.com>
2022-12-16 21:06:15 +0900

    feat(InputView): 자동차 이름 입력 view 로직 구현

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,5 +1,12 @@
+const { Console } = require("@woowacourse/mission-utils");
+const { CAR_NAME } = require("../constants/Messages");
+
 const InputView = {
-  readInput() {},
+  readCarNamesInput(callbackFunction) {
+    Console.readLines(CAR_NAME.INPUT, (carNamesInput) => {
+      callbackFunction(carNamesInput);
+    });
+  },
 };
 
 module.exports = InputView;
