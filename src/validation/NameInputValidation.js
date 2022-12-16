5a0684382a2c17fc762f2f6e6db1e2dc35a05234
sh981013s <sh981013s@gmail.com>
2022-12-16 21:00:51 +0900

    feat(NameInputValidation):자동차 이름 유효성 검사 로직 구현

--- src/validation/NameInputValidation.js
+++ src/validation/NameInputValidation.js
@@ -1,5 +1,14 @@
+const { CAR_NAME } = require("../constants/Messages");
+
 class NameInputValidation {
-  static validate() {}
+  static validate(carNameInput) {
+    const splitCarNameInput = carNameInput.split(",");
+    for (const singleCarName of splitCarNameInput) {
+      if (!singleCarName || singleCarName.length > 5) {
+        throw new Error(CAR_NAME.ERROR);
+      }
+    }
+  }
 }
 
 module.exports = NameInputValidation;
