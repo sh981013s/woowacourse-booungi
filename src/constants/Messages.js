7500251d7e3c8b310bfffd0d8b0361d53801907c
sh981013s <sh981013s@gmail.com>
2022-12-16 20:57:03 +0900

    feat(Messages): 게임 내 에러 메세지 문 상수화

--- src/constants/Messages.js
+++ src/constants/Messages.js
@@ -1,9 +1,12 @@
 const CAR_NAME = Object.freeze({
   INPUT: "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n",
+  ERROR:
+    "[ERROR] 자동차 이름은 쉼표를 기준으로 구분해야하며, 이름은 5자 이하만 가능합니다.",
 });
 
 const TRY_COUNT = Object.freeze({
   INPUT: "시도할 회수는 몇회인가요?\n",
+  ERROR: "[ERROR] 시도 횟수는 숫자여야 합니다",
 });
 
 const CASE_RESULT = Object.freeze({
