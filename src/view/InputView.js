f980192f360b96a3d1237f6a3db9d37630869fea
sh981013s <sh981013s@gmail.com>
2022-12-16 21:35:51 +0900

    feat(InputView): 시도 회수 입력 View 로직 구현

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,10 +1,17 @@
 const { Console } = require("@woowacourse/mission-utils");
-const { CAR_NAME } = require("../constants/Messages");
+const { CAR_NAME,
+  TRY_COUNT } = require("../constants/Messages");
 
 const InputView = {
   readCarNamesInput(callbackFunction) {
     Console.readLine(CAR_NAME.INPUT, (carNamesInput) => {
       callbackFunction(carNamesInput);
+    });
+  },
+
+  readTryCount(callbackFunction) {
+    Console.readLine(TRY_COUNT.INPUT, (tryCountInput) => {
+      callbackFunction(tryCountInput);
     });
   },
 };
