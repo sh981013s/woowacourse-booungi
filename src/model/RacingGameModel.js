c3a77191d1909a761aad05aea3afe315129041f9
sh981013s <sh981013s@gmail.com>
2022-12-16 22:10:19 +0900

    feat(RacingGameModel): 자동차 생성 메서드 구현

--- src/model/RacingGameModel.js
+++ src/model/RacingGameModel.js
@@ -1,5 +1,17 @@
+const Car = require("./Car");
+
 class RacingGameModel {
-  constructor() {}
+  #cars;
+
+  constructor() {
+    this.#cars = [];
+  }
+
+  generateCars(carNames) {
+    for (const singleCarName of carNames) {
+      this.#cars.push(new Car(singleCarName));
+    }
+  }
 }
 
 module.exports = RacingGameModel;
