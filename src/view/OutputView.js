ac35c484faddd16e9a6182cc9c663829a97290dc
sh981013s <sh981013s@gmail.com>
2022-12-16 21:19:50 +0900

    refactor(OutputView): 기존 class => 객체 형태로 변환

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,9 +1,9 @@
 const { Console } = require("@woowacourse/mission-utils");
 
-class OutputView {
+const OutputView = {
   printError(errorLog) {
     Console.print(errorLog.message);
-  }
-}
+  },
+};
 
 module.exports = OutputView;
