ea15fb14bf9d48a909ad3eccab3a1758ffb71084
sh981013s <sh981013s@gmail.com>
2022-12-16 21:53:49 +0900

    feat(Car): Car 객체 움직임 시도 메서드 및 정보 getter 메서드 구현

--- src/model/Car.js
+++ src/model/Car.js
@@ -1,6 +1,20 @@
+const IsPossibleToMove = require("../utils/IsPossibleToMove");
+
 class Car {
+  #name;
+  #movement;
+
   constructor(name) {
-    this.name = name;
+    this.#name = name;
+    this.#movement = 0;
+  }
+
+  getCarInformation() {
+    return { name: this.#name, movement: this.#movement };
+  }
+
+  tryMove() {
+    if (IsPossibleToMove.isPossible()) this.#movement++;
   }
 }
 
